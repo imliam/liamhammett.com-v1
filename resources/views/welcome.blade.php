23b7a4b4e266c74b9af48a83dadc8ffe68330f5c
I'm Liam <liamhammett@gmail.com>
2018-03-06 00:01:09 +0000

    Update job and add Medium link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,8 +53,7 @@
             <div id="tagline" class="text-grey-lightest font-hairline hidden py-4">
                 <p>
                     <span id="greeting-small" class="hidden">Hi, I'm Liam.</span>
-                    I work at <a href="https://microtest.co.uk/" class="text-grey no-underline hover:text-grey-lighter">Microtest</a> creating healthcare software.
-                    {{-- I work at <a href="https://helmsquared.com/" class="text-grey no-underline hover:text-grey-lighter">Helm²</a> building ticketing software. --}}
+                    I work at <a href="https://helmsquared.com/" class="text-grey no-underline hover:text-grey-lighter">Helm²</a> building ticketing software.
                 </p>
                 <p class="text-sm pt-2">I also make a lot of awful jokes. Sorry about that.</p>
             </div>
@@ -75,13 +74,11 @@
                 </svg>
             </a>
 
-            {{--
             <a href="https://medium.com/@liamhammett" class="text-grey-darkest hover:text-medium py-2 px-2 no-underline" data-balloon="Medium" data-balloon-pos="down">
                 <svg aria-hidden="true" alt="Medium" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-8">
                     <path fill="currentColor" d="M0 32v448h448V32H0zm372.2 106.1l-24 23c-2.1 1.6-3.1 4.2-2.7 6.7v169.3c-.4 2.6.6 5.2 2.7 6.7l23.5 23v5.1h-118V367l24.3-23.6c2.4-2.4 2.4-3.1 2.4-6.7V199.8l-67.6 171.6h-9.1L125 199.8v115c-.7 4.8 1 9.7 4.4 13.2l31.6 38.3v5.1H71.2v-5.1l31.6-38.3c3.4-3.5 4.9-8.4 4.1-13.2v-133c.4-3.7-1-7.3-3.8-9.8L75 138.1V133h87.3l67.4 148L289 133.1h83.2v5z"></path>
                 </svg>
             </a>
-            --}}
 
             <a href="mailto:liam@liamhammett.com" class="text-grey-darkest hover:text-grey-darker py-2 px-2 no-underline" data-balloon="E-mail" data-balloon-pos="down">
                 <svg aria-hidden="true" alt="E-mail" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-8">
